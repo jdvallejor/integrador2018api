b62b718a1c907971468c1e0f94cde14adfcc8069
jhonemu <jhonmurillousuga33@gmail.com>
2018-04-01 14:17:12 -0500

    jenkins test

--- src/main/java/com/psl/integrador/IntegradorApplication.java
+++ src/main/java/com/psl/integrador/IntegradorApplication.java
@@ -2,7 +2,7 @@ package com.psl.integrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// a coment
 @SpringBootApplication
 public class IntegradorApplication {
 
